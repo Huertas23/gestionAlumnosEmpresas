55bdcf4357fccaa770367a64370f36463a4bf1dc
Jesús Huertas García <147037934+Huertas23@users.noreply.github.com>
2024-06-06 15:25:07 +0200

    Add files via upload

--- gestionAlumnoEmpresa.sql
+++ gestionAlumnoEmpresa.sql
@@ -1,4 +1,5 @@
-CREATE TABLE empresas (
+-- Crear tabla empresa
+CREATE TABLE empresa (
     id INT AUTO_INCREMENT PRIMARY KEY,
     razon_social VARCHAR(255) NOT NULL,
     domicilio_social VARCHAR(255),
@@ -11,36 +12,48 @@ CREATE TABLE empresas (
     dni_representante VARCHAR(50)
 );
 
-CREATE TABLE alumnos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR(255) NOT NULL,
-    dni VARCHAR(50),
-    centro_practicas VARCHAR(255),
-    empresa_id INT,
-    FOREIGN KEY (empresa_id) REFERENCES empresas(id)
-);
-
-ALTER TABLE alumnos ADD COLUMN tutor_centro_id INT;
-ALTER TABLE alumnos ADD FOREIGN KEY (tutor_centro_id) REFERENCES tutores(id);
-
-
-
-CREATE TABLE tutores (
+-- Crear tabla tutor
+CREATE TABLE tutor (
     id INT AUTO_INCREMENT PRIMARY KEY,
     curso VARCHAR(50),
     nombre VARCHAR(255) NOT NULL,
     dni_responsable VARCHAR(50)
 );
 
+-- Crear tabla alumno
+CREATE TABLE alumno (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    apellido VARCHAR(255) NOT NULL,
+    dni VARCHAR(50),
+    centro_practicas VARCHAR(255),
+    empresa_id INT,
+    tutor_centro_id INT,
+    FOREIGN KEY (tutor_centro_id) REFERENCES tutor(id)
+);
 
 CREATE TABLE seguimiento (
     id INT AUTO_INCREMENT PRIMARY KEY,
     alumno_id INT,
-    tutor_id INT,
+    id_empresa INT,  -- Asegurar longitud coincide con empresa
     fecha_seguimiento DATE,
     informe_final TEXT,
-    archivo_pdf VARCHAR(255),  -- Nueva columna para almacenar la ruta del archivo PDF
-    FOREIGN KEY (alumno_id) REFERENCES alumnos(id),
-    FOREIGN KEY (tutor_id) REFERENCES tutores(id)
+    archivo_pdf VARCHAR(255)  -- Nueva columna para almacenar la ruta del archivo PDF
 );
+
+-- Agregar clave foránea para alumno_id
+ALTER TABLE seguimiento
+ADD CONSTRAINT fk_alumno
+FOREIGN KEY (alumno_id) REFERENCES alumno(id);
+
+-- Agregar clave foránea para dni_tutor_laboral
+ALTER TABLE seguimiento
+ADD CONSTRAINT fk_id_empresa
+FOREIGN KEY (id_empresa) REFERENCES empresa(id);
+
+-- Crear tabla usuario
+CREATE TABLE usuario (
+    nombre VARCHAR(50),
+    password VARCHAR(50)
+);
+
